8eef076d9b049adc230f3a0ddf0b932b6a21cea3
VitorMioto <VitorMioto@users.noreply.github.com>
2019-08-23 00:41:52 -0300

    Criação classe empregado

---
+++ LINQ/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ.Entities
+{
+    class Employee
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+
+        public Employee (string nome, string email, double salario)
+        {
+            Nome = nome;
+            Email = email;
+            Salario = salario;
+        }
+    }
+}
